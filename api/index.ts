d2836b2925d66efb1936eb94ee331f0d6a76ab02
0xmsr <148552427+0xmsr@users.noreply.github.com>
2026-01-14 07:40:10 +0700

    Update import paths for Blockchain and Transaction

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { Blockchain } from "../blockchain";
-import { Transaction } from "../transaction";
+import { Blockchain } from "./blockchain";
+import { Transaction } from "./transaction";
 import { Block } from "../block";
 
 const app = express();
